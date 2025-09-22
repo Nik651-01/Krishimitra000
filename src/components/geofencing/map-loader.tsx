9b4b40414019afd0ab54491e7197dd692bae749c
Nikhil Kumar <nk2514249@gmail.com>
2025-09-22 14:44:12 +0000

    Error: Map container is already initialized.     at NewClass._initContai

--- src/components/geofencing/map-loader.tsx
+++ src/components/geofencing/map-loader.tsx
@@ -1,13 +1,12 @@
 'use client';
 
 import dynamic from 'next/dynamic';
-import { useMemo } from 'react';
+
+const InteractiveMap = dynamic(
+    () => import('@/components/geofencing/interactive-map'),
+    { ssr: false }
+);
 
 export function MapLoader() {
-    const InteractiveMap = useMemo(() => dynamic(
-        () => import('@/components/geofencing/interactive-map'),
-        { ssr: false }
-    ), []);
-
     return <InteractiveMap />;
 }
